3797ff49ecbdeb505a58b35b789f0b1282f931b4
Chunhawit <pupz28@gmail.com>
2021-01-02 18:17:46 +0100

    styling app theme

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App Hola',
+      title: 'Personal Expenses',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+        accentColor: Colors.amberAccent,
+      ),
       home: MyHomePage(),
     );
   }
@@ -66,7 +70,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Personal Expenses'),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
